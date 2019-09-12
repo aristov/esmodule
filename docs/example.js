9dd39b1482232c2803e1e356e5d95b5cb9d2ee83
aristov <vv.aristov@gmail.com>
2019-09-12 12:48:13 +0300

    Docs: add example

---
+++ docs/example.js
@@ -0,0 +1,54 @@
+import { Assembler } from '../lib'
+
+class HTMLAssembler extends Assembler {
+    set children(children) {
+        this.node.append(...children.map(child => child.node || child))
+    }
+
+    get children() {
+        return this.node.children
+    }
+
+    get node() {
+        return HTMLAssembler.getTargetOf(this)
+    }
+
+    static create(init) {
+        return document.createElement(this.name.toLowerCase())
+    }
+
+    static get defaultPropertyName() {
+        return 'children'
+    }
+
+    static get targetPropertyName() {
+        return 'node'
+    }
+}
+
+class Body extends HTMLAssembler {}
+class H1 extends HTMLAssembler {}
+class P extends HTMLAssembler {}
+class Form extends HTMLAssembler {}
+class Label extends HTMLAssembler {}
+class Input extends HTMLAssembler {}
+class Button extends HTMLAssembler {}
+
+new Body({
+    node : document.body,
+    children : [
+        new H1(['Hello world!']),
+        new P(['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']),
+        new Form([
+            new Label([
+                'Name ',
+                new Input({
+                    name : 'name',
+                    value : (new URL(location)).searchParams.get('name')
+                })
+            ]),
+            ' ',
+            new Button(['Confirm!'])
+        ])
+    ]
+})
